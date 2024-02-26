c96c6c9c3c8581a79798c7bcfbb377a4896eb018
Goraniiii <gony06011@gmail.com>
2024-02-26 21:19:28 +0900

    feat: MyPageClient 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
+import './pages/index';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='/mypage' element={<MyPageClient/>} />
       </Routes>
     </BrowserRouter>
   );
