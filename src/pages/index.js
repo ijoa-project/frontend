c195b8a21b406121e024b99f14bb61eb1ddebb68
Goun Park <104720260+Goraniiii@users.noreply.github.com>
2024-02-26 21:27:11 +0900

    feat: MyPageClient 추가 (#1)

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+const glob = require('glob');
+const path = require('path');
+
+const folder = '.src/pages/';
+const select = '!(index)';
+const format = '.jsx';
+const local = folder + select + format;
+
+module.export = {};
+glob.sync(local).forEach(file => {
+	const name = path.basename(file, format);
+	const m = require ('./' + name).default;
+	module.exports[m.name] = m;
+});
