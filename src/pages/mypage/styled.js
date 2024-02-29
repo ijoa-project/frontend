3f0e45dd1a911bf9e0aa7775ef3b8326a2c8ba82
Goraniiii <gony06011@gmail.com>
2024-02-29 17:11:27 +0900

    feat: mypage styled 수정

--- src/pages/mypage/styled.js
+++ src/pages/mypage/styled.js
@@ -1,4 +1,4 @@
-import { styled, Box, Grid } from '@mui/material'
+import { styled, Box, Grid, ButtonBase, ListItemText } from '@mui/material'
 
 export const Container = styled(Grid)(() => ({
   display: 'flex',
@@ -8,15 +8,16 @@ export const Container = styled(Grid)(() => ({
 }))
 
 export const Profile = styled(Box)(() => ({
-  margin: '90px 0px 10px 120px',
+  margin: '150px 120px 10px 120px',
   display: 'flex',
-  alignItems: 'center',
+  alignItems: 'flex-end',
+  // justifyContent: 'space-between',
 }))
 
 export const Section = styled(Box)(() => ({
   display: 'inline-block',
   border: '1px solid #d4d2e3',
-  width: 500,
+  width: 350,
   height: 160,
   borderRadius: 15,
   margin: 20,
@@ -26,4 +27,32 @@ export const ProfileImg = styled('img')(() => ({
   borderRadius: 9999,
   width: 170,
   height: 170,
+}))
+
+export const Button = styled(ButtonBase)(() => ({
+  fontSize: 16,
+  margin: 10,
+  color: 'gray',
+  alignItems: 'center',
+}))
+
+export const SectionTitle = styled(Box)(() => ({
+  color: '#5d5a88',
+  fontWeight: 700,
+  paddingTop: 20,
+  paddingLeft: 30,
+  paddingBottom: 10,
+  textAlign: 'left',
+}))
+
+export const List = styled(Grid)(() => ({
+  paddingBottom: 17,
+  textAlign: 'left',
+  // justifyContent: 'start',
+}))
+
+export const ListItem = styled(Grid)(() => ({
+  lineHeight: '30px',
+  marginLeft: 35,
+  cursor: 'pointer',
 }))
