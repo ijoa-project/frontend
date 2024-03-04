2a5f2b76f5d7ced16eaba170397645d63fe1c324
hyeyeonismm <hyeyeonismm@gmail.com>
2024-03-04 18:33:42 +0900

    feat: 헤더 구현

--- src/components/common/styled.js
+++ src/components/common/styled.js
@@ -1,54 +1,74 @@
 import { styled } from 'styled-components'
-
-// export const Container = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   flex-direction: column;
-//   padding: 100px 15vw 30px 15vw;
-
-//   // Desktop
-//   @media (min-width: 768px) {
-//     justify-content: space-around;
-//     flex-direction: row;
-//   }
-// `
+import { IoIosArrowDown } from 'react-icons/io'
 
 export const Frame = styled.div`
-  background-color: white;
   display: flex;
-  padding: 20px 30px;
+  padding: 20px 8vw 20px 4vw;
 `
 
-export const Title = styled.div`
-  font-weight: 700;
-  flex: 1;
-  font-size: 22px;
+export const Title = styled.button`
+  font-weight: 900;
+  flex: 0.5;
+  font-size: 24px;
+  border: none;
+  background-color: transparent;
   color: #5d5a88;
   cursor: pointer;
   &:hover {
-    background-color: #fff;
-    cursor: pointer;
-  }
-  &:active {
-    background-color: transparent;
-    box-shadow: none;
+    opacity: 0.5;
   }
 `
 
 export const Contents = styled.div`
   display: flex;
+  justify-content: flex-end;
+  gap: 8vw;
   flex: 2;
 `
 
 export const LoginBtn = styled.button`
-  text-align: center;
-  margin: -8px 20px 0px 12px;
-  font-size: 16px;
-  font-weight: 600;
-  border-radius: 40px;
   background-color: #5d5a88;
+  padding: 10px;
   color: #fff;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  font-size: 16px;
   &:hover {
-    background-color: #8d8ba7;
+    opacity: 0.5;
+  }
+`
+export const Menu = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: #5d5a88;
+  font-size: 16px;
+`
+
+export const ArrowIcon = styled(IoIosArrowDown)`
+  color: #5d5a88;
+  margin-left: 3px;
+  transform: translateY(3.5px);
+`
+
+export const Dropdown = styled.div`
+  margin-top: 10px;
+  margin-left: -10px;
+  position: fixed;
+  width: 120px;
+  border: 1px solid #ddd;
+  border-radius: 20px;
+  padding: 5px;
+`
+export const Item = styled.button`
+  border: none;
+  background-color: transparent;
+  color: #9795b5;
+  display: flex;
+  cursor: pointer;
+  padding: 5px 10px;
+  &:hover {
+    opacity: 0.5;
   }
 `
