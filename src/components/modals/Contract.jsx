f8cf7161837b72ff47d8350ffd4dfebcfd4fd1cd
Goraniiii <gony06011@gmail.com>
2024-03-21 00:40:17 +0900

    feat: Contract 공통 컴포넌트 구현

---
+++ src/components/modals/Contract.jsx
@@ -0,0 +1,149 @@
+import styled from 'styled-components'
+import UserIcon from 'assets/images/icon_user.png'
+import CalenderIcon from 'assets/images/icon_calendar.png'
+import ClockIcon from 'assets/images/icon_clock.png'
+import NoteIcon from 'assets/images/icon_notepad.png'
+
+export function Contract(props) {
+  const contract = props.data
+  const disable = props.inputDisabled
+
+  return (
+    <>
+      <Row>
+        <Wrapper>
+          <IconImg draggable={false} src={UserIcon} />
+          <InputElement>
+            <InputTitle>작성자</InputTitle>
+            <Input style={{ width: 150 }} placeholder="작성자" value={'작성자 by ID'} disabled={disable.parent} />
+          </InputElement>
+        </Wrapper>
+        <Wrapper>
+          <IconImg draggable={false} src={UserIcon} />
+          <InputElement>
+            <InputTitle>선생님</InputTitle>
+            <Input style={{ width: 150 }} placeholder="선생님" value={'돌보미 by ID'} disabled={disable.dolbomi} />
+          </InputElement>
+        </Wrapper>
+      </Row>
+      <Row>
+        <Wrapper>
+          <IconImg draggable={false} src={CalenderIcon} />
+          <InputElement>
+            <InputTitle>시작날짜</InputTitle>
+            <Input
+              style={{ width: 150 }}
+              placeholder="시작날짜"
+              value={contract.date[0]}
+              disabled={disable.startDate}
+            />
+          </InputElement>
+        </Wrapper>
+        <Wrapper>
+          <IconImg draggable={false} src={CalenderIcon} />
+          <InputElement>
+            <InputTitle>종료날짜</InputTitle>
+            <Input
+              style={{ width: 150 }}
+              placeholder="종료날짜"
+              value={contract.date[contract.date.length - 1]}
+              disabled={disable.endDate}
+            />
+          </InputElement>
+        </Wrapper>
+      </Row>
+      <Row>
+        <Wrapper>
+          <IconImg draggable={false} src={ClockIcon} />
+          <InputElement>
+            <InputTitle>시작시간</InputTitle>
+            <Input
+              style={{ width: 150 }}
+              placeholder="시작시각"
+              value={contract.startTime}
+              disabled={disable.startTime}
+            />
+          </InputElement>
+        </Wrapper>
+        <Wrapper>
+          <IconImg draggable={false} src={ClockIcon} />
+          <InputElement>
+            <InputTitle>종료시간</InputTitle>
+            <Input style={{ width: 150 }} placeholder="종료시각" value={contract.endTime} disabled={disable.endTime} />
+          </InputElement>
+        </Wrapper>
+      </Row>
+      <Row>
+        <Wrapper>
+          <IconImg draggable={false} src={CalenderIcon} />
+          <InputElement>
+            <InputTitle>돌봄장소</InputTitle>
+            <Input style={{ width: 450 }} placeholder="주소" value={contract.region} disabled={disable.region} />
+          </InputElement>
+        </Wrapper>
+      </Row>
+      <Row>
+        <Wrapper>
+          <IconImg draggable={false} src={NoteIcon} />
+          <InputElement>
+            <InputTitle>돌봄유형</InputTitle>
+            <Input
+              style={{ width: 450 }}
+              placeholder="돌봄유형"
+              value={contract.careType}
+              disabled={disable.careType}
+            />
+          </InputElement>
+        </Wrapper>
+      </Row>
+    </>
+  )
+}
+
+const Input = styled('input')(() => ({
+  height: '22px',
+  padding: 16,
+  background: '#FCFCFD',
+  borderRadius: 12,
+  border: '2px #DFDFE6 solid',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  display: 'inline-flex',
+  Color: '#87898E',
+  fontSize: 16,
+  lineHeight: 16,
+}))
+
+const InputTitle = styled('div')(() => ({
+  color: '#87898E',
+  fontSize: 16,
+  lineHeight: '16px',
+  textAlign: 'left',
+  width: '100%',
+  marginBottom: 5,
+  marginLeft: 3,
+}))
+
+const InputElement = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+}))
+
+const Wrapper = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+}))
+
+const IconImg = styled('img')(() => ({
+  width: 35,
+  height: 35,
+  marginTop: 32,
+  marginRight: 10,
+  draggable: false,
+}))
+
+const Row = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  marginBottom: 20,
+}))
--- src/components/modals/ContractConfirmModal.jsx
+++ src/components/modals/ContractConfirmModal.jsx
@@ -1,9 +1,6 @@
 import { Modal, ModalWindow, TitleSection, ModalTitle, ModalCloseButton, ModalDetail } from './Modal'
-import styled from 'styled-components'
-import UserIcon from 'assets/images/icon_user.png'
-import CalenderIcon from 'assets/images/icon_calendar.png'
-import ClockIcon from 'assets/images/icon_clock.png'
-import NoteIcon from 'assets/images/icon_notepad.png'
+
+import { Contract } from './Contract'
 
 export function ContractConfirmModal(props) {
   const contract = {
@@ -19,6 +16,16 @@ export function ContractConfirmModal(props) {
     careType: ['놀이돌봄', '교육돌봄'],
     totalCost: '40000',
   }
+  const inputDisabled = {
+    parent: true,
+    dolbomi: true,
+    startDate: true,
+    endDate: true,
+    startTime: true,
+    endTime: true,
+    region: true,
+    careType: true,
+  }
 
   return (
     <Modal open={props.open} handleClose={props.handleClose}>
@@ -28,121 +35,8 @@ export function ContractConfirmModal(props) {
           <ModalCloseButton onClick={props.handleClose} />
         </TitleSection>
         <ModalDetail>지난 돌봄 내역을 확인하실 수 있습니다.</ModalDetail>
-        <div>
-          <div style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: 20 }}>
-            <Wrapper>
-              <IconImg src={UserIcon} />
-              <InputElement>
-                <InputTitle>작성자</InputTitle>
-                <Input style={{ width: 150 }} placeholder="작성자" value={'작성자 by ID'} disabled={true} />
-              </InputElement>
-            </Wrapper>
-            <Wrapper>
-              <IconImg src={UserIcon} />
-              <InputElement>
-                <InputTitle>선생님</InputTitle>
-                <Input style={{ width: 150 }} placeholder="선생님" value={'돌보미 by ID'} disabled={true} />
-              </InputElement>
-            </Wrapper>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: 20 }}>
-            <Wrapper>
-              <IconImg src={CalenderIcon} />
-              <InputElement>
-                <InputTitle>시작날짜</InputTitle>
-                <Input style={{ width: 150 }} placeholder="시작날짜" value={contract.date[0]} disabled={true} />
-              </InputElement>
-            </Wrapper>
-            <Wrapper>
-              <IconImg src={CalenderIcon} />
-              <InputElement>
-                <InputTitle>종료날짜</InputTitle>
-                <Input
-                  style={{ width: 150 }}
-                  placeholder="종료날짜"
-                  value={contract.date[contract.date.length - 1]}
-                  disabled={true}
-                />
-              </InputElement>
-            </Wrapper>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: 20 }}>
-            <Wrapper>
-              <IconImg src={ClockIcon} />
-              <InputElement>
-                <InputTitle>시작시간</InputTitle>
-                <Input style={{ width: 150 }} placeholder="시작시각" value={contract.startTime} disabled={true} />
-              </InputElement>
-            </Wrapper>
-            <Wrapper>
-              <IconImg src={ClockIcon} />
-              <InputElement>
-                <InputTitle>종료시간</InputTitle>
-                <Input style={{ width: 150 }} placeholder="종료시각" value={contract.endTime} disabled={true} />
-              </InputElement>
-            </Wrapper>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: 20 }}>
-            <Wrapper>
-              <IconImg src={CalenderIcon} />
-              <InputElement>
-                <InputTitle>돌봄장소</InputTitle>
-                <Input style={{ width: 450 }} placeholder="주소" value={contract.region} disabled={true} />
-              </InputElement>
-            </Wrapper>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: 20 }}>
-            <Wrapper>
-              <IconImg src={NoteIcon} />
-              <InputElement>
-                <InputTitle>돌봄유형</InputTitle>
-                <Input style={{ width: 450 }} placeholder="돌봄유형" value={contract.careType} disabled={true} />
-              </InputElement>
-            </Wrapper>
-          </div>
-        </div>
+        <Contract data={contract} inputDisabled={inputDisabled} />
       </ModalWindow>
     </Modal>
   )
 }
-
-const Input = styled('input')(() => ({
-  height: '22px',
-  padding: 16,
-  background: '#FCFCFD',
-  borderRadius: 12,
-  border: '2px #DFDFE6 solid',
-  justifyContent: 'flex-end',
-  alignItems: 'center',
-  display: 'inline-flex',
-  Color: '#87898E',
-  fontSize: 16,
-  lineHeight: 16,
-}))
-
-const InputTitle = styled('div')(() => ({
-  color: '#87898E',
-  fontSize: 16,
-  lineHeight: '16px',
-  textAlign: 'left',
-  width: '100%',
-  marginBottom: 5,
-  marginLeft: 3,
-}))
-
-const InputElement = styled('div')(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-}))
-
-const Wrapper = styled('div')(() => ({
-  display: 'flex',
-  flexDirection: 'row',
-}))
-
-const IconImg = styled('img')(() => ({
-  width: 35,
-  height: 35,
-  marginTop: 32,
-  marginRight: 10,
-}))
